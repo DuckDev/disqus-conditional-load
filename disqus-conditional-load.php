12b8969589d7d05e996a43bb62f568110176afbb
Joel James <me@joelsays.com>
2016-08-25 20:03:01 +0530

    Update disqus-conditional-load.php

--- disqus-conditional-load.php
+++ disqus-conditional-load.php
@@ -1,1 +1,37 @@
 <?php
+/**
+ * Plugin Name:     Disqus Conditional Load
+ * Plugin URI:      https://dclwp.com
+ * Description:     Best Diqus plugin for WordPress comments with advanced features like <strong>lazy loading, shortcode, widgets, Woocommerce/EDD support</strong> etc.
+ * Version:         3.0.0
+ * Author:          Joel James
+ * Author URI:      https://thefoxe.com/
+ * Donate link:     https://paypal.me/JoelCJ
+ * License:         GPL-2.0+
+ * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:     disqus-conditional-load
+ * Domain Path:     /languages
+ *
+ * Disqus Conditional Load is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Disqus Conditional Load is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Disqus Conditional Load. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @category Core
+ * @package  DCL
+ * @author   Joel James <me@joelsays.com>
+ * @license  http://www.gnu.org/licenses/ GNU General Public License
+ * @link     https://dclwp.com
+ */
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die( 'Damn it.! Dude you are looking for what?' );
+}
