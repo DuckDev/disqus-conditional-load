8cf9091a7bdafcc16c72d68d368c2a50026082fe
Joel James <me@joelsays.com>
2017-02-18 02:40:28 +0530

    Added classes

---
+++ classes/core/api/class-cli.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DCL\API;
+
+// If this file is called directly, abort.
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Disqus CLI class for command execution.
+ *
+ * This class is responsible for executing commandline actions
+ * for importing and exporting.
+ *
+ * @since	 11.0.0
+ * @category API
+ * @package  DCL\API\CLI
+ * @author   Joel James <me@joelsays.com>
+ * @license  http://www.gnu.org/licenses/ GNU General Public License
+ */
+class CLI {
+
+	
+}
